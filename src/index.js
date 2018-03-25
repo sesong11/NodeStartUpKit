86686380a51525448a385b4535a916afc2863978
DESKTOP-1SQF2N0\seson <sesong11@gmail.com>
2018-03-25 23:46:58 +0700

    mock api using json-server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import {getUsers} from './api/userApi';
+import './index.css';
+
+import {getUsers, deleteUser} from './api/userApi';
 
 getUsers().then(result => {
   let usersBody = "";
@@ -14,4 +16,18 @@ getUsers().then(result => {
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  //Must use array.from to create a real array from a DOM collection
+  //getElementsByClassName only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    }
+  });
 });
