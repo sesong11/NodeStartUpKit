54a073dda5afd7a06b2bb3f12e4ddc41b82b438e
DESKTOP-1SQF2N0\seson <sesong11@gmail.com>
2018-04-05 20:46:08 +0700

    Production Build

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,11 @@
 export default function getBaseUrl() {
-  const inDevelopment = window.location.hostname === 'localhost';
-  return inDevelopment ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+}
+
+function getQueryStringParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+  return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
